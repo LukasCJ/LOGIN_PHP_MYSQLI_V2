f2f4038397cfb38cb303a57acdb9cc07a2238684
LukasCJ <lukas.johansson3@elev.ga.ntig.se>
2022-06-10 21:35:18 +0200

    2022-06-10

--- sql/for_db.sql
+++ sql/for_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE `users` (
     `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `name` varchar(128) NOT NULL,
     `email` varchar(128) NOT NULL,
@@ -6,62 +6,78 @@ CREATE TABLE users (
     `pwd` varchar(128) NOT NULL
 );
 
-CREATE TABLE items (
-    -- universella:
-    `type` varchar(128) NOT NULL CHECK (`type` IN ('Film', 'Short Film', 'Game', 'Series', 'Mini Series')), -- Film, ShortFilm, Game, Series, MiniSeries
+CREATE TABLE `items` (
+    `type` varchar(128) NOT NULL CHECK (`type` IN ('Film', 'Short Film', 'Game', 'Series', 'Mini Series')),
     `id` int(11) NOT NULL,
     `name` varchar(128) NOT NULL,
     `date` date NOT NULL,
-    `poster_path` varchar(128) NOT NULL,
-    `bg_path` varchar(128) NOT NULL,
+    `poster-path` varchar(128),
+    `bg-path` varchar(128),
     `description` text NOT NULL,
     `rating` float(2) NOT NULL DEFAULT 0,
-    `popularity_all` int(11) NOT NULL DEFAULT 0,
-    `popularity_week` int(11) NOT NULL DEFAULT 0,
+    `popularity-all` int(11) NOT NULL DEFAULT 0,
+    `popularity-week` int(11) NOT NULL DEFAULT 0,
 
-    -- `related` json NOT NULL, -- inkluderar både items i samma franchise och liknande items
-    -- `cast` json NOT NULL,
-    -- `crew` json NOT NULL,
+    -- endast för spel och filmer (i minuter)
+    -- spels längd hämtad från howlongtobeat.com
+    `length` int(5), 
 
-    `length` int(5), -- för spel och filmer, i minuter. spels längd hämtad från howlongtobeat.com
-
-    `series_length_seasons` int(3),
-    `series_length_eps` int(5),
+    -- endast för serier
+    `series_length-seasons` int(3),
+    `series_length-eps` int(5),
     `series_ongoing` bit, -- 1: yes
-    `series_date_last` date -- NULL if ongoing
+    `series_date-last` date -- NULL if ongoing
 );
 
-CREATE TABLE seasons (
+CREATE TABLE `seasons` (
     `number` int(4) NOT NULL,
-    `series_id` varchar(128) NOT NULL,
+    `series_id` int(11) NOT NULL,
     `length_eps` int(5) NOT NULL,
     `date` date NOT NULL,
-    `bg_path` varchar(128) NOT NULL,
+    `bg_path` varchar(128),
     `description` varchar(128) NOT NULL,
 );
 
-CREATE TABLE eps (
+CREATE TABLE `eps` (
     `number_of_season` int(5) NOT NULL, -- vilket avsnitt av denna säsongen?
     `number_of_series` int(5) NOT NULL, -- vilket avsnitt av hela serien?
-    `series_id` varchar(128) NOT NULL,
-    `season` varchar(128) NOT NULL, -- vilken säsong
+    `series_id` int(11) NOT NULL,
+    `season` varchar(128) NOT NULL, -- vilken säsong?
     `name` varchar(128) NOT NULL,
     `date` date NOT NULL,
-    `bg_path` varchar(128) NOT NULL,
+    `bg_path` varchar(128),
     `description` varchar(128) NOT NULL,
     `length` int(5) NOT NULL,
     `rating` float(2) NOT NULL DEFAULT 0
 );
 
-CREATE TABLE ratings (
+CREATE TABLE `edition` (
+    `item_id` int(11) NOT NULL,
+    `name` varchar(128) NOT NULL,
+    `length` int(5), 
+);
+
+CREATE TABLE `alt_editions` (
+    
+);
+
+CREATE TABLE `achievements` (
+    `name`
+);
+
+CREATE TABLE `unlocked_achievements` (
+    `name`
+);
+
+CREATE TABLE `ratings` (
     `user_id` int(11) NOT NULL,
     `item_type` varchar(128) NOT NULL,
     `item_id` int(11) NOT NULL,
-    `rating` float(2), -- mellan 1 och 5. halvpoäng funkar. om 0 => NULL, räknas inte med i avg. 
+    `rating` float(2), -- mellan 0.1 och 5. tiondels poäng funkar. om 0 => NULL, räknas inte med i avg. 
     `like` bit NOT NULL
 );
 
-CREATE TABLE entries (
+CREATE TABLE `entries` (
     `user_id` int(11) NOT NULL,
     `item_type` varchar(128) NOT NULL,
     `item_id` int(11) NOT NULL,
